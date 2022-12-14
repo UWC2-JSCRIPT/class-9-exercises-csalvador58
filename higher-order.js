669475b217634401b0dc2c22e5de918e79262c96
Chris Salvador <csalvador58@gmail.com>
2022-12-14 00:27:56 -0800

    Repeat function problem completed.

--- higher-order.js
+++ higher-order.js
@@ -1,7 +1,12 @@
 // This function should execute the callback function the number of times specified.
 // When the function is being executed, the repetition number (i.e. 1 for the first call)
 // should be passed to the callback.
-const repeatFn = (times, callback) => {};
+const repeatFn = (times, callback) => {
+  for (let i = 0; i < times; i++) {
+    callback(i);
+  }
+};
+
 
 // Test repeatFn
 const addButton = num => {
@@ -10,6 +15,8 @@ const addButton = num => {
   document.querySelector('body').appendChild(button);
 };
 repeatFn(6, addButton);
+
+
 
 const toThePower = (num, pow) => {
   let product = 1;
